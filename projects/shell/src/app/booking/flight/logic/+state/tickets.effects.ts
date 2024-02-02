fc13265e29653574109822d7f2c14d14af1a1c21
mikezks <23002661+mikezks@users.noreply.github.com>
2024-02-02 13:29:50 +0100

    feat: effects comments

--- projects/shell/src/app/booking/flight/logic/+state/tickets.effects.ts
+++ projects/shell/src/app/booking/flight/logic/+state/tickets.effects.ts
@@ -1,20 +1,34 @@
 import { Injectable, inject } from "@angular/core";
-import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { Actions, concatLatestFrom, createEffect, ofType } from "@ngrx/effects";
 import { FlightService } from "../data-access/flight.service";
 import { ticketActions } from "./tickets.actions";
-import { map, switchMap } from "rxjs";
+import { map, switchMap, withLatestFrom } from "rxjs";
+import { Store } from "@ngrx/store";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketsEffects {
   private actions$ = inject(Actions);
+  private store = inject(Store);
   private flightService = inject(FlightService);
 
   loadFlights = createEffect(
+    /**
+     * Stream 1: Action
+     *  - Trigger
+     *  - State Provider: from, to (City names)
+     */
     () => this.actions$.pipe(
+      // Filtering: Action
       ofType(ticketActions.flightsLoad),
-      switchMap(action => this.flightService.find(action.from, action.to)),
+      concatLatestFrom(() => this.store),
+      /**
+       * Stream 2: HTTP Backend API Call
+       *  - State Provider: Array of Flights
+       */
+      switchMap(([action, state]) => this.flightService.find(action.from, action.to)),
+      // Transformation: Flights -> Action
       map(flights => ticketActions.flightsLoaded({ flights}))
     )
   );
